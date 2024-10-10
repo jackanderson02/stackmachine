416fd0374c2274d9bca2758dce339956d2b40a42
Jack Anderson <jack.anderson@bjss.com>
2024-10-10 10:04:18 +0100

    Add pop function and struct

--- stackmachine.go
+++ stackmachine.go
@@ -1,6 +1,22 @@
 package main
 
-import "errors"
+
+import(
+	"errors"
+	"slices"
+)
+
+type Stack struct{
+	StackNumbers []int
+}
+
+func (stack *Stack) Pop() int{
+	lastElementIndex := len(stack.StackNumbers) -1
+	lastElement := stack.StackNumbers[lastElementIndex]
+	stack.StackNumbers = slices.Delete(stack.StackNumbers, lastElementIndex, lastElementIndex)
+
+	return lastElement
+}
 
 func StackMachine(commands string)(int, error) {
 	return 0, errors.New("")
